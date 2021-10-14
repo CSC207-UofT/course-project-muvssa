16ed8a21d87bd65727cada8c27a2f99a4bce54cc
abdullaaaah <abdullaaaah_@outlook.com>
2021-10-14 16:43:45 -0400

    Adds the Comment and Post classes.

---
+++ src/main/java/Comment.java
@@ -0,0 +1,36 @@
+public class Comment {
+    private User user;
+    private String content;
+
+    /**
+     * This constructor generates a new comment
+     * @param user - The user that wrote the comment
+     * @param content - the string content of the comment
+     */
+    public Comment(User user, String content)
+    {
+        this.user = user;
+        this.content = content;
+    }
+
+    /**
+     * Getter method for content
+     * @return content of the comment
+     */
+    public String getContent() {
+        return this.content;
+    }
+
+    /**
+     * Setter method for content
+     * @param content - the new content to replace current content with
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+
+
+
+}
