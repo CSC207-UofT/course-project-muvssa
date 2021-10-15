c3644153448a967024081ab9d30199d89373e2f6
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2021-10-14 23:56:37 -0400

    Fix ProfilesTest tests and add description attribute to Routine class

--- src/main/java/Routine.java
+++ src/main/java/Routine.java
@@ -2,45 +2,70 @@ import java.util.ArrayList;
 
 public class Routine {
     private String name;
+    private String description;
     private ArrayList<Workout> workouts;
 
     /**
      * A constructor that creates a Routine given a name and list of workouts
-     * @param name      the name of the Routine
-     * @param workouts  the list of workouts
+     *
+     * @param name     the name of the Routine
+     * @param workouts the list of workouts
      */
-    public Routine(String name, ArrayList<Workout> workouts) {
+    public Routine(String name, String description, ArrayList<Workout> workouts) {
         this.name = name;
+        this.description = description;
         this.workouts = workouts;
     }
 
     /**
      * Gets the name
-     * @return  name of the Routine
+     *
+     * @return name of the Routine
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Gets the list of workouts
-     * @return  list of workouts for the Routine
-     */
-    public ArrayList<Workout> getWorkouts() {
-        return workouts;
-    }
-
-    /**
      * Sets the name of the Routine to something new
-     * @param name  the new name of Routine
+     *
+     * @param name the new name of Routine
      */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
+     * Gets the description
+     *
+     * @return description of the Routine
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the description to a given one
+     *
+     * @param description new description for the Routine
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Gets the list of workouts
+     *
+     * @return list of workouts for the Routine
+     */
+    public ArrayList<Workout> getWorkouts() {
+        return workouts;
+    }
+
+    /**
      * Sets the Routine's workouts to new ones given
-     * @param workouts  list of new workouts
+     *
+     * @param workouts list of new workouts
      */
     public void setWorkouts(ArrayList<Workout> workouts) {
         this.workouts = workouts;
@@ -48,6 +73,7 @@ public class Routine {
 
     /**
      * Add a workout to the Routine
+     *
      * @param workout new workout to be added
      */
     public void addWorkout(Workout workout) {
@@ -56,6 +82,7 @@ public class Routine {
 
     /**
      * Remove a workout if the given name matches any of the workout names from the Routine
+     *
      * @param name name of the workout to be removed from Routine's workouts
      */
     public void removeWorkout(String name) {
@@ -69,4 +96,14 @@ public class Routine {
         this.workouts.clear();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Routine))
+            return false;
+        Routine routine = (Routine) o;
+        return name.equals(routine.name) && description.equals(routine.description) &&
+                workouts.equals(routine.workouts);
+    }
 }
