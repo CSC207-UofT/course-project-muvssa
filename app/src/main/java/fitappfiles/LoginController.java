2682cbac8d8e2c6ec6681f096e6604612ea0735e
Abdullah Shahid <nxabdullah1@gmail.com>
2021-11-05 02:27:56 -0400

    Creates loginController

---
+++ app/src/main/java/fitappfiles/LoginController.java
@@ -0,0 +1,18 @@
+package fitappfiles;
+
+public class LoginController {
+
+    /* The collection of all profiles */
+    private Profiles profiles;
+
+    /**
+     * Constructs a new LoginUseCase object
+     * @param profiles: All the profiles that belong in the app
+     */
+    public LoginController(Profiles profiles)
+    {
+        this.profiles = profiles;
+    }
+
+
+}
