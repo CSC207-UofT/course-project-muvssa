adce1eb4723b9bbb733d28c67fcb1a6b7c7a1e7b
sanasarin <87511101+sanasarin@users.noreply.github.com>
2021-11-12 14:01:48 -0500

    made test cases for the user class

---
+++ app/src/test/java/fitappfiles/UserTest.java
@@ -0,0 +1,39 @@
+package fitappfiles;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+    User user_1;
+
+    public void setUp() throws Exception {
+        user_1 = new User("damnsana", "abcd", "abcd");
+
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testGetUsername() {
+        assertEquals("damnsana", user_1.getUsername());
+    }
+
+    public void testGetPassword() {
+        assertEquals("abcd", user_1.getPassword());
+    }
+
+    public void testGetEmail() {
+        assertEquals("abcd", user_1.getEmail());
+    }
+
+    public void testSetPassword() {
+        user_1.setPassword("abc");
+        assertEquals("abc", user_1.getPassword());
+    }
+
+    public void testLogin() {
+        assertTrue(user_1.login("damnsana", "abcd"));
+        assertFalse(user_1.login("damnsana", "djhfskj"));
+        assertFalse(user_1.login("shsh", "abcd"));
+    }
+}
