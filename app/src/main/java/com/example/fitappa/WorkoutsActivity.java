3ea632c755061458a18d7461ce8be23882d72a46
Abdullah Shahid <nxabdullah1@gmail.com>
2021-11-13 02:32:45 -0500

    Adds the Workouts Activity.

---
+++ app/src/main/java/com/example/fitappa/WorkoutsActivity.java
@@ -0,0 +1,13 @@
+package com.example.fitappa;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkoutsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workouts2);
+    }
+}
