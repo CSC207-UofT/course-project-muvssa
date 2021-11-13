4f1f853dda64ed4dea4f3bbc19af85f2d7509e36
sanasarin <87511101+sanasarin@users.noreply.github.com>
2021-11-12 21:10:38 -0500

    Made layouts for a few more classes (test-cases) if anyone wants to work on them.

---
+++ app/src/test/java/fitappfiles/WeeklyScheduleTest.java
@@ -0,0 +1,31 @@
+package fitappfiles;
+
+import junit.framework.TestCase;
+
+public class WeeklyScheduleTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testGetWeeklySchedule() {
+    }
+
+    public void testGetWorkout() {
+    }
+
+    public void testAddWorkout() {
+    }
+
+    public void testRemoveWorkout() {
+    }
+
+    public void testChangeWorkout() {
+    }
+
+    public void testClearSchedule() {
+    }
+}
