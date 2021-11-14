0233182f7bf796e67b231b46e764cc97380f0bb1
Abdullah Shahid <nxabdullah1@gmail.com>
2021-11-14 15:01:23 -0500

    Adds Sign In Activity

---
+++ app/src/main/java/com/example/fitappa/View/SignInActivity.java
@@ -0,0 +1,13 @@
+package com.example.fitappa.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in2);
+    }
+}
