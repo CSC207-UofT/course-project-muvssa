e005d52f5d69ad338efcce0cdc1f4f6175460f73
Abdullah Shahid <nxabdullah1@gmail.com>
2021-11-13 21:50:48 -0500

    Fixes some of Layout Bug

--- app/src/main/java/com/example/fitappa/View/WorkoutsActivity.java
+++ app/src/main/java/com/example/fitappa/View/WorkoutsActivity.java
@@ -1,7 +1,10 @@
 package com.example.fitappa.View;
 
 import android.content.Intent;
+import android.widget.Button;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.fitappa.Model.Routine;
@@ -23,17 +26,44 @@ public class WorkoutsActivity extends AppCompatActivity implements WorkoutsActiv
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workouts2);
 
+        // Load the elements
+        routinesView = findViewById(R.id.routinesView);
+
         // Get the profile
         this.profile = (Profile) retrieveIntent.getSerializableExtra("my_Profile");
 
         // We must initialize the Presenter in the View.
         this.presenter = new WorkoutsActivityPresenter(this, profile);
 
+        // Test
+        this.updateRoutinesView(profile.getRoutines());
+
+
     }
 
 
     @Override
     public void updateRoutinesView(ArrayList<Routine> routines) {
-        
+        for(Routine routine : routines) {
+
+            LinearLayout myLayout = findViewById(R.id.routinesLayout);
+
+            Button button = new Button(this);
+            button.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT));
+
+            button.setText(routine.getName());
+
+
+            TableRow row = new TableRow(this);
+            row.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT));
+
+            row.addView(button);
+
+            routinesView.addView(row);
+        }
     }
 }
