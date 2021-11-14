1625b374585549b18ad5eb6c5b94866724dc52c6
Abdullah Shahid <nxabdullah1@gmail.com>
2021-11-13 19:57:25 -0500

    Adds Empty CreateWorkout and AddExercise Activity

---
+++ app/src/main/java/com/example/fitappa/View/CreateWorkoutActivity.java
@@ -0,0 +1,15 @@
+package com.example.fitappa.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.fitappa.R;
+
+
+public class CreateWorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_workout);
+    }
+}
