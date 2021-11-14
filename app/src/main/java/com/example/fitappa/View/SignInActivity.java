d746cacc19d5f0deac763da70ef2baa4eaaa40b2
Abdullah Shahid <nxabdullah1@gmail.com>
2021-11-14 15:03:53 -0500

    Adds Sign In Activity pt 3

--- app/src/main/java/com/example/fitappa/View/SignInActivity.java
+++ app/src/main/java/com/example/fitappa/View/SignInActivity.java
@@ -2,6 +2,7 @@ package com.example.fitappa.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.fitappa.R;
 
 public class SignInActivity extends AppCompatActivity {
 
