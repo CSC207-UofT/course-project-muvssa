90b6c61cff10be327cde655b448cb6eec16f0523
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2021-11-14 21:06:10 -0500

    Manual merge of the other branch

---
+++ app/src/main/java/com/example/fitappa/View/ViewRoutineActivity.java
@@ -0,0 +1,51 @@
+package com.example.fitappa.View;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.fitappa.Model.Entity.Workout;
+import com.example.fitappa.Model.UseCase.Profile;
+import com.example.fitappa.Model.UseCase.Routine;
+import com.example.fitappa.R;
+
+import java.util.ArrayList;
+
+public class ViewRoutineActivity extends AppCompatActivity {
+    private Routine routine;
+    private LinearLayout routineLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_routine);
+
+        // Retrieve UI
+        this.routineLayout = findViewById(R.id.RoutineLayout);
+
+        // retrieve routine
+        this.routine = (Routine) getIntent().getSerializableExtra("routineObj");
+
+        // Initialize view
+        initializeRoutineView(this.routine.getWorkouts());
+
+    }
+
+    public void updateRoutineView(Workout workout) {
+        Button button = new Button(this);
+        button.setText(workout.getName());
+        Log.d("WORKOUT NAME", workout.getName());
+        routineLayout.addView(button);
+
+    }
+
+    public void initializeRoutineView(ArrayList<Workout> workouts) {
+        for(Workout w : workouts) {
+            updateRoutineView(w);
+        }
+    }
+
+
+}
