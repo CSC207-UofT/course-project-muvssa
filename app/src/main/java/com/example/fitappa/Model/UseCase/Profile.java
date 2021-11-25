e357770e35da8b7e07ec648324b0bd939b38b0a5
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2021-11-25 00:09:32 -0500

    Add DefaultExercises.java and implement in Profile.java

--- app/src/main/java/com/example/fitappa/Model/UseCase/Profile.java
+++ app/src/main/java/com/example/fitappa/Model/UseCase/Profile.java
@@ -1,6 +1,7 @@
 package com.example.fitappa.Model.UseCase;
 
 
+import com.example.fitappa.Model.Entity.DefaultExercises;
 import com.example.fitappa.Model.Entity.User;
 import com.example.fitappa.Model.Gateway.Saveable;
 
@@ -14,6 +15,7 @@ public class Profile implements Serializable {
     private FollowManager followManager;
     private List<Routine> routines;
     private Saveable gateway;
+    private DefaultExercises defaultExercises;
 
     /**
      * Creates the main profile for one user
@@ -25,6 +27,7 @@ public class Profile implements Serializable {
         this.followManager = new FollowManager(this.user);
         this.routines = new ArrayList<>();
         this.gateway = gateway;
+        this.defaultExercises = new DefaultExercises();
     }
 
     // empty constructor necessary for Firebase
@@ -42,6 +45,15 @@ public class Profile implements Serializable {
      */
     public void setRoutines(List<Routine> routines) {
         this.routines = routines;
+    }
+
+    /**
+     * Get the object containing default exercises for this profile
+     *
+     * @return DefaultExercises object containing a list of exercises
+     */
+    public DefaultExercises getDefaultExercises() {
+        return defaultExercises;
     }
 
     /**
@@ -72,13 +84,6 @@ public class Profile implements Serializable {
     }
 
     /**
-     * Save this profile to a database through the gateway
-     */
-    public void saveData() {
-        gateway.save(this);
-    }
-
-    /**
      * Gets who the User is following and who is following them
      *
      * @return returns a HashMap of people User is following
@@ -94,5 +99,13 @@ public class Profile implements Serializable {
      */
     public void addRoutine(Routine r) {
         this.routines.add(r);
+    }
+
+
+    /**
+     * Save this profile to a database through the gateway
+     */
+    public void saveData() {
+        gateway.save(this);
     }
 }
