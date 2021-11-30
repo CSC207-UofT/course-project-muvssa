f27dd33fd042a34f349a43b15965aa4afe165871
sourenrex <souren.rex@gmail.com>
2021-11-30 18:26:24 -0500

    Added a settings page

---
+++ app/src/main/java/com/example/fitappa/Profile/SettingPresenter.java
@@ -0,0 +1,95 @@
+package com.example.fitappa.Profile;
+
+/**
+ * This class is a presenter class for ViewSettingActivity
+ * <p>
+ * The methods in this class help move information to and from the ViewSettingActivity class
+ *
+ * @author Souren
+ * @since 0.1
+ */
+
+class SettingPresenter {
+    private final Profile profile;
+    private final View view;
+
+    /**
+     * A constructor for SettingPresenter
+     *
+     * @param view    the ViewSettingActivity
+     * @param profile a users Profile
+     */
+    SettingPresenter(View view, Profile profile) {
+        this.view = view;
+        this.profile = profile;
+    }
+
+    /**
+     * retrieve user's weight
+     * @return string of their weight
+     */
+    String retrieveWeight() {
+        return profile.getUserWeight();
+    }
+
+    /**
+     * retrieve user's height
+     * @return string of their height
+     */
+    String retrieveHeight() {
+        return profile.getUserHeight();
+    }
+
+    /**
+     * retrieve a user's first name
+     * @return string of their first name
+     */
+    String retrieveFirstName() {
+        return profile.getUserFirstName();
+    }
+
+    /**
+     * retrieve a user's last name
+     * @return string of their last name
+     */
+    String retrieveLastName() {
+        return profile.getUserLastName();
+    }
+
+    /**
+     * Will change a users extra information like weight, height, first name, and last name. It will also not change
+     * anything if the user hasn't made any edits.
+     * @param w user inputted string weight
+     * @param h user inputted string height
+     * @param f user inputted string first name
+     * @param l user inputted string last name
+     */
+    void changeSettings(String w,String h, String f, String l){
+        if(w.equals("")){
+            w = profile.getUserWeight();
+        }
+        if (h.equals("")){
+            h = profile.getUserHeight();
+        }
+        if(f.equals("")){
+            f = profile.getUserFirstName();
+        }
+        if(l.equals("")){
+            l = profile.getUserLastName();
+        }
+        profile.setUserExtraInfo(w,h,f,l);
+        view.update();
+    }
+
+    interface View {
+        /**
+         * Display the changes the user made to their settings
+         */
+        void update();
+
+        /**
+         * return to profile after changes made
+         */
+        void toProfile();
+    }
+}
