4909f884c6fe30cfc44eb689215e8be1d0645dec
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2021-12-01 00:33:24 -0500

    Add javadoc for DatabaseConstants

--- app/src/main/java/com/example/fitappa/Authentication/DatabaseConstants.java
+++ app/src/main/java/com/example/fitappa/Authentication/DatabaseConstants.java
@@ -1,16 +1,34 @@
 package com.example.fitappa.Authentication;
 
+/**
+ * Constants to be used for Firebase methods
+ */
 public class DatabaseConstants {
 
+    /**
+     * Get the collection name for users
+     *
+     * @return String representing collection name for users
+     */
+    public String getUsersCollection() {
+        return "users";
+    }
+
+    /**
+     * Get the collection name for exercises
+     *
+     * @return String representing collection name for exercises
+     */
     public String getExercisesCollection() {
         return "exercises";
     }
 
+    /**
+     * Get the name for the document that contains username
+     *
+     * @return String representing name for the document that contains username
+     */
     public String getUsernameDocument() {
         return "user.username";
-    }
-
-    public String getUsersCollection() {
-        return "users";
     }
 }
