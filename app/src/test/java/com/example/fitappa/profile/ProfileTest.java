23ba476ab42047565577d9317d1accab7f43f764
Munim Adil <munim.adil@mail.utoronto.ca>
2021-12-08 19:59:59 -0500

    Tests for profile objects. To be finalized

--- app/src/test/java/com/example/fitappa/profile/ProfileTest.java
+++ app/src/test/java/com/example/fitappa/profile/ProfileTest.java
@@ -1,43 +1,88 @@
 package com.example.fitappa.profile;
 
-import static org.junit.Assert.assertEquals;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class ProfileTest {
     Profile profile1;
     Profile profile2;
-
-
     @Before
     public void setUp() throws Exception {
-        this.profile1 = new Profile("johnnyappleseed@gmail.com", "Johnny", "");
+        //two profile objects are made
+        profile1 = new Profile("johnnyappleseed@gmail.com", "Johnny", "101290");
+        profile2 = new Profile("helloworld@gmail.com", "Hello", "239092");
 
-        this.profile2 = new Profile("helloworld@gmail.com", "Hello", "");
 
     }
 
+    @After
+    public void tearDown() throws Exception {
+    }
+
     @Test
-    public void testGetEmail() {
+    public void getEmail() {
+        //sees if the desired string matches the one that's got
         assertEquals("johnnyappleseed@gmail.com", profile1.getEmail());
+        assertEquals("helloworld@gmail.com", profile2.getEmail());
+    }
+    @Test
+    public void getUsername() {
+        //sees if the desired string matches the one that's got
+        assertEquals("Johnny", profile1.getUsername());
+        assertEquals("Hello", profile2.getUsername());
     }
 
     @Test
-    public void testGetRoutines() {
-        // shows that it matches the elements of the
-        // hard coded routines
-//        assertEquals(routine1, profile1.getRoutines().get(0));
-//        assertEquals(routine2, profile2.getRoutines().get(0));
+    public void getUniqueID() {
+        //sees if the desired string matches the one that's got
+        assertEquals("101290", profile1.getUniqueID());
+        assertEquals("239092", profile2.getUniqueID());
     }
 
     @Test
-    public void testAddRoutine() {
-        // shows that the element has been added by looking at
-        // size and matching name
-//        profile2.addRoutine(new Routine("My routine3"));
-//        assertEquals("My routine3", profile2.getRoutines().get(1).getName());
-//        assertEquals(2, profile2.getRoutines().size());
+    public void setgetWeight() {
+        //sees if the desired string matches the one that's set and gotten
+        profile1.setWeight("140");
+        profile2.setWeight("160");
+        assertEquals("140", profile1.getWeight());
+        profile1.setWeight(profile2.getWeight());
+        assertEquals("160", profile1.getWeight());
+    }
 
+    @Test
+    public void setgetHeight() {
+        //sees if the desired string matches the one that's set and gotten
+        profile1.setHeight("75");
+        profile2.setHeight("90");
+        assertEquals("75", profile1.getHeight());
+        assertEquals("90", profile2.getHeight());
+        profile2.setHeight(profile1.getHeight());
+        assertEquals("75", profile2.getHeight());
+    }
+
+    @Test
+    public void setgetFirstName() {
+        //sees if the desired string matches the one that's set and gotten
+        profile1.setFirstName("John");
+        assertEquals("John", profile1.getFirstName());
+        profile1.setFirstName("Jack");
+        assertEquals("Jack", profile1.getFirstName());
+        profile2.setFirstName(profile1.getFirstName());
+        assertNotEquals("John", profile2.getFirstName());
+    }
+
+    @Test
+    public void setgetLastName() {
+        //sees if the desired string matches the one that's set and gotten
+        profile1.setFirstName("John");
+        assertNull(profile1.getLastName());
+        profile1.setLastName("Apple");
+        assertEquals("Apple", profile1.getLastName());
+        profile2.setLastName("World");
+        profile2.setLastName(profile1.getFirstName());
+        assertEquals("John", profile2.getLastName());
     }
 }
